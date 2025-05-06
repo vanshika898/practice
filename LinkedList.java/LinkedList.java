14127a5f1b72a420f2d5197869d5e3b9ba1e83d7
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-06 19:53:32 +0530

    Node creation

---
+++ LinkedList.java/LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
