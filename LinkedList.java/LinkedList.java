d087350f06ffb0a77127093ee60cf092169ae31c
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-07 18:59:51 +0530

    addFirst fuction added

--- LinkedList.java/LinkedList.java
+++ LinkedList.java/LinkedList.java
@@ -1,6 +1,31 @@
 public class LinkedList {
-    public static void main(String[] args) {
+    public static Node head = null;
+    public static Node tail = null;
 
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            tail = head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void printLinkedList() {
+        Node act = head;
+        while (act != null) {
+            System.out.print(act.data + "->");
+            act = act.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(20);
+        ll.addFirst(10);
+        ll.printLinkedList();
     }
 }
 
