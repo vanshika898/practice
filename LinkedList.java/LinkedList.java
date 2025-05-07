a21b489eb73527917d890c501aca26b110027ed0
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-07 21:47:57 +0530

    till remove last

--- LinkedList.java/LinkedList.java
+++ LinkedList.java/LinkedList.java
@@ -12,6 +12,72 @@ public class LinkedList {
         head = newNode;
     }
 
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            tail = head = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void addInBetween(int index, int data) {
+        Node newNode = new Node(data);
+        Node act = head;
+        int i = 0;
+        while (i < index - 1) {
+            act = act.next;
+        }
+        newNode.next = act.next;
+        act.next = newNode;
+    }
+
+    public int removeFirst() {
+        if (head == null) {
+            return -1;
+        } else if (head.next == null) {
+            int cv = head.data;
+            head = null;
+            return cv;
+        } else {
+            int cv = head.data;
+            head = head.next;
+            return cv;
+        }
+
+    }
+
+    public int size() {
+        Node act = head;
+        int size = 0;
+        while (act != null) {
+            act = act.next;
+            size++;
+        }
+        return size;
+    }
+
+    public int removeLast() {
+        if (head == null) {
+            return -1;
+        } else if (head.next == null) {
+            int cv = head.data;
+            head = null;
+            return cv;
+        }
+        Node act = head;
+
+        while (act.next.next != null) {
+            act = act.next;
+
+        }
+        int a = act.next.data;
+        act.next = null;
+        return a;
+
+    }
+
     public void printLinkedList() {
         Node act = head;
         while (act != null) {
@@ -25,7 +91,14 @@ public class LinkedList {
         LinkedList ll = new LinkedList();
         ll.addFirst(20);
         ll.addFirst(10);
+        ll.addLast(30);
+        ll.addInBetween(1, 15);
         ll.printLinkedList();
+        ll.removeFirst();
+        ll.printLinkedList();
+        ll.removeLast();
+        ll.printLinkedList();
+
     }
 }
 
