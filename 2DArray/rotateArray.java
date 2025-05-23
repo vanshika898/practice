6561d19b354eb4821eb2f128cf93b20488b39e88
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-23 23:11:21 +0530

    day8

---
+++ 2DArray/rotateArray.java
@@ -0,0 +1,33 @@
+
+
+public class rotateArray {
+    public static void rotate2DArray(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                int temp  = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+
+            }
+        }
+
+    }
+    public static void printArray(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String [] args){
+        int  arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        printArray(arr);
+        System.out.println("====================================");
+        rotate2DArray(arr);
+         System.out.println("====================================");
+        printArray(arr);
+
+    }
+    
+}
