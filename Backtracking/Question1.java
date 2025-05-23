5326efd0755589a68a512ac85040f2ab7a866bbf
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-23 19:27:31 +0530

    day8

---
+++ Backtracking/Question1.java
@@ -0,0 +1,25 @@
+// Understanding the concept of backtracking
+
+public class Question1 {
+    public static void changeArray(int arr[],int i,int val){
+        if(i==arr.length){
+         printArray(arr);
+            return;
+        }
+        //kaam 
+        arr[i]=val;
+        changeArray(arr, i+1, val+1);
+        arr[i]= val-2;
+    }
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String [] args){
+        int arr[] = new int[5];
+        changeArray(arr, 0, 1);
+        printArray(arr);
+    }
+}
