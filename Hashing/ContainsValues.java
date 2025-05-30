1701dbb41e238579e5182df05cc4a8255c080987
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-30 23:49:03 +0530

    day13

---
+++ Hashing/ContainsValues.java
@@ -0,0 +1,20 @@
+
+import java.util.HashMap;
+
+
+
+public class ContainsValues {
+    public static void main(String[] args) {
+        HashMap<String,Integer>hm1 = new HashMap<>();
+
+        hm1.put("india", 300);
+        hm1.put("japan",200);
+
+        if(hm1.containsValue(300)){
+            System.out.println("yes");
+
+        }else{
+            System.out.println("no");
+        }
+    }
+}
