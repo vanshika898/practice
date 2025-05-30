beb429aebede080696d82df8bde3a0f58072eeb6
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-31 00:05:43 +0530

    day14

---
+++ Hashing/giveValues.java
@@ -0,0 +1,13 @@
+
+import java.util.HashMap;
+public class giveValues {
+    public static void main(String[] args) {
+        HashMap <String,Integer>hm1 = new HashMap<>();
+        hm1.put("india",300);
+        hm1.put("ujjain",500);
+
+
+       System.out.println(hm1.values());
+    }
+    
+}
