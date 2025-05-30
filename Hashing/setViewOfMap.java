a6b7a391d1c62c858ec9e1894c254972758d7e4d
vanshika898 <vanshikanigam8989@gmail.com>
2025-05-30 23:55:21 +0530

    day13

---
+++ Hashing/setViewOfMap.java
@@ -0,0 +1,20 @@
+
+import java.util.HashMap;
+import java.util.Set;
+
+
+
+public class setViewOfMap {
+
+    private static boolean s11;
+    public static void main(String[] args) {
+        HashMap<String,Integer>hm1 = new HashMap<>();
+        hm1.put("india",200);
+        hm1.put("USA",300);
+
+        Set s1= hm1.entrySet();
+        System.out.println(s1);
+        
+    }
+    
+}
