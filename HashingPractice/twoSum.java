00dc8b13563faeeee8bf48e875d07445cc071c67
vanshika898 <vanshikanigam8989@gmail.com>
2025-06-02 23:56:35 +0530

    day15

---
+++ HashingPractice/twoSum.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class twoSum {
+    public static void sumTwo(int arr[],int target){
+        HashSet<Integer>hm = new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            int com = target-arr[i];
+             if(hm.contains(com)){
+                 System.out.println("Pair found: (" + arr[i] + ", " + com + ")");
+             }
+             hm.add(com);
+        }
+       
+        
+        
+    }
+    public static void main(String args []){
+        int arr[] = {1,2,1,3,4,6};
+        int target = 10;
+        sumTwo(arr, target);
+       
+
+    }
+    
+}
