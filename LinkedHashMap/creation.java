25f3b2f4c73903e35d7be22530c2b16181c80651
vanshika898 <vanshikanigam8989@gmail.com>
2025-06-02 21:42:54 +0530

    day15

---
+++ LinkedHashMap/creation.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashMap;
+//doubly linkedlist
+public class creation {
+    public static void main(String args[]){
+
+     LinkedHashMap<String,Integer> lhm = new LinkedHashMap<>();
+     lhm.put("India", 200);
+      lhm.put("USA", 300);
+       lhm.put("Japan", 400);
+
+
+       System.out.println(lhm);
+
+
+    }
+}
