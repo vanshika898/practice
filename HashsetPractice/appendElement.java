b7882200fe49305cf760f15b90bfcad6f8a11d27
vanshika898 <vanshikanigam8989@gmail.com>
2025-06-03 21:59:45 +0530

    day16

---
+++ HashsetPractice/appendElement.java
@@ -0,0 +1,15 @@
+//Append element in hashset
+import java.util.*;
+
+public class appendElement {
+    public static void main(String[] args) {
+        HashSet <Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+        s1.add(4);
+
+        System.out.println(s1);
+    }
+    
+}
