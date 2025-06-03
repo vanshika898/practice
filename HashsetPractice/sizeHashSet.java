cca7c17d512e59f4ebe923da05f47e98c3f4b7c7
vanshika898 <vanshikanigam8989@gmail.com>
2025-06-03 22:16:09 +0530

    day16

---
+++ HashsetPractice/sizeHashSet.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class sizeHashSet {
+    public static void main(String args[]){
+    HashSet<Integer>s1 = new HashSet<>();
+s1.add(2);
+s1.add(3);
+s1.add(4);
+s1.add(5);
+System.out.println(s1.size());
+
+
+}
+}
