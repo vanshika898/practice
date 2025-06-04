7a4381604bb4a923b04a099168320c3e88df628c
vanshika898 <vanshikanigam8989@gmail.com>
2025-06-04 15:36:20 +0530

    day17

---
+++ Generics/practice1.java
@@ -0,0 +1,19 @@
+public class practice1 {
+    public static void main(String[] args) {
+       Rupay<Integer> r= new Rupay(12) ;
+        r.prints();
+
+        Rupay<String> r1= new Rupay("vanshika") ;
+        r1.prints();
+    }
+}
+class  Rupay<T>{
+   public  T value;
+    Rupay(T value){
+        this.value = value;
+    }
+    public void prints(){
+        System.out.println("Value is :"+value);
+    }
+    
+}
