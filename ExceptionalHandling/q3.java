c159df56db803bcb475419abe7a693206ab1b816
vanshika898 <vanshikanigam8989@gmail.com>
2025-06-09 23:32:24 +0530

    day21

---
+++ ExceptionalHandling/q3.java
@@ -0,0 +1,16 @@
+public class q3 {
+
+
+    public static int trybl(){
+         try{
+            
+            return 1;
+        }finally{
+            return 2;
+        }
+    }
+    public static void main(String args[]){
+      System.out.println(  trybl());
+    }
+    }
+
