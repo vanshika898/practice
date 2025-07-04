edcc295362f026975b12ce854e4febc142709933
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-04 16:45:16 +0530

    day31(Stream sorting)

--- Streams/SortingString.java
+++ Streams/SortingString.java
@@ -15,8 +15,13 @@ public class SortingString {
        // Fruit.stream().sorted(Comparator.naturalOrder()).forEach(System.out::println);
 
        //change into list 
-       List <String> sorted = Fruit.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
-       System.out.println(sorted);
+       //List <String> sorted = Fruit.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+       //System.out.println(sorted);
+         // if make decscending
+       Fruit.stream().sorted((a1,a2)->a2.compareTo(a1)).forEach(System.out::println);
+       System.out.println("-------------------------------------------------------------");
+
+       Fruit.stream().sorted().forEach(System.out::println);
 
 
     }
