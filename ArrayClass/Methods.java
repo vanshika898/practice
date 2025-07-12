11358393d665e4574dab32b6a2db6bf0930bef3f
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-12 22:37:39 +0530

    day35

---
+++ ArrayClass/Methods.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+// Example: Demonstrating the use of the Arrays class with the equals() and toString() methods.
+public class Methods {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4};
+        int b[] = {1,2,3,4};
+
+        System.out.println(Arrays.equals(a, b));
+        System.out.println(Arrays.toString(a));
+         System.out.println(Arrays.toString(b));
+
+         
+    }
+    
+}
